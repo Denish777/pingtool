d74f46bd3db89a2ae35a20de7c522452e8d9656e
Mr./K5NC1L5 <43570272+Denish777@users.noreply.github.com>
2019-05-23 11:36:23 +0700

    Add files via upload

---
+++ ping.py
@@ -0,0 +1,29 @@
+import os
+banner = """
+
+
+██████╗ ██╗███╗   ██╗ ██████╗     ████████╗ ██████╗  ██████╗ ██╗     
+██╔══██╗██║████╗  ██║██╔════╝     ╚══██╔══╝██╔═══██╗██╔═══██╗██║     
+██████╔╝██║██╔██╗ ██║██║  ███╗       ██║   ██║   ██║██║   ██║██║     
+██╔═══╝ ██║██║╚██╗██║██║   ██║       ██║   ██║   ██║██║   ██║██║     
+██║     ██║██║ ╚████║╚██████╔╝       ██║   ╚██████╔╝╚██████╔╝███████╗
+╚═╝     ╚═╝╚═╝  ╚═══╝ ╚═════╝        ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝
+                                                                     
+
+              Speed Internet - Coded By: Mr./K4NC1L5
+==================================================================
+|    Sfx* Ni Tool Cmn Buat Stabilin Sinyal Lo Doang Ya Panteq    |
+|         recommended Buat Maen Game Online Biar Ping Lu Stabil  |
+|                   TTD Admin Gans: Denis.H >;v                  |
+==================================================================
+
+"""
+hijau="\033[0;32m"
+
+os.system("clear")
+print(hijau+banner)
+
+input('Tekan Enter Untuk Memulai...')
+
+import os
+os.system ("ping google.com")
